7d60e641d03386355289332dab3eed2618f806a4
BlueSkyFish <blueskyfish@blueskyfish.de>
2015-08-17 20:53:59 +0200

    server: separeted the current values from the sensor from the fixed sensor name

---
+++ server/sql/update-tables.0.0.4.sql
@@ -0,0 +1,42 @@
+--
+-- temperature-monitoring - http://github.com/blueskyfish/temperature-monitoring.git
+--
+-- The MIT License (MIT)
+-- Copyright (c) 2015 BlueSkyFish
+--
+
+--
+-- Update the Tables to Version 0.0.4
+--
+
+DROP TABLE IF EXISTS `sensor-currents`;
+CREATE TABLE `sensor-currents` (
+  `group_id`    INT NOT NULL,
+  `name_id`     INT NOT NULL,
+  `temperature` INT NOT NULL COMMENT 'current temperature from sensor'  DEFAULT '0',
+  `humidity`    INT NOT NULL COMMENT 'current humidity from sensor'  DEFAULT '0'
+) ENGINE = InnoDB COMMENT = '(temperature monitoring) the current temperature and humidity';
+
+--
+-- Indizes für die Tabelle `sensor-currents`
+--
+ALTER TABLE `sensor-currents`
+ADD PRIMARY KEY (`group_id`,`name_id`) USING BTREE;
+
+--
+-- Insert the sensor
+--
+INSERT INTO `sensor-currents` (`group_id`, `name_id`) VALUES ('1000', '1');
+
+
+-- ----------------------------------------------------------------------------
+
+--
+-- Delete the field `sensor-names.temperature`
+--
+ALTER TABLE `sensor-names` DROP `temperature`;
+
+--
+-- Delete the field `sensor-names.humidity`
+--
+ALTER TABLE `sensor-names` DROP `humidity`;
